5ce7faa2aaf5759947bdc54621251ad814e54134
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-26 00:46:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  list = attendees
+  attendees.push(badge_maker(name))
+  list
+end
