50de028034cc3680e741dedd304014ae89c537ad
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-26 01:49:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,9 @@ end
 
 def assign_rooms(attendees)
   room_assignments = []
-  attendees.each_with_index { |attendee, index| room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!") }
+  attendees.each_with_index do |attendee, index|
+    room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
+  end
   room_assignments
 end
 
